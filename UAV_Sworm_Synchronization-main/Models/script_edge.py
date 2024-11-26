d1212a5b423d293b7403be0c8d11b1ec998f984e
Shreyansh Dubey <dshrey27@gmail.com>
2024-11-27 02:40:45 +0530

    Added option to choose b/w mode

--- UAV_Sworm_Synchronization-main/Models/script_edge.py
+++ UAV_Sworm_Synchronization-main/Models/script_edge.py
@@ -2,32 +2,23 @@ import pandas as pd
 import numpy as np
 from keras.models import load_model
 
-model_path_ANN = "ANN\model.h5"  
-model_path_CNN = "ANN\model.h5"  
-model_ANN = load_model(model_path_ANN)
-model_CNN = load_model(model_path_CNN)
+model_choice = input("Choose a model to use (ANN or CNN): ").strip().upper()
 
-input_dataset_path_ANN = "ANN\dataset-1.csv"
-input_dataset_path_CNN = "CNN\dataset-1.csv"
+if model_choice == "ANN":
+    model_path = "ANN\\model.h5"
+    input_dataset_path = "ANN\\dataset-1.csv"
+    output_dataset_path = "ANN\\output_ann.csv"
+elif model_choice == "CNN":
+    model_path = "CNN\\model.h5"
+    input_dataset_path = "CNN\\dataset-1.csv"
+    output_dataset_path = "CNN\\output_cnn.csv"
+else:
+    raise ValueError("Invalid model choice. Please choose 'ANN' or 'CNN'.")
 
-data_ANN = pd.read_csv(input_dataset_path_ANN)
-data_CNN = pd.read_csv(input_dataset_path_CNN)
-
-X_ANN = data_ANN.values
-X_ANN = X_ANN[:, :52]
-
-X_CNN = data_CNN.values
-X_CNN = X_CNN[:, :52]
-
-predictions_ANN = model_ANN.predict(X_ANN)
-predictions_CNN = model_CNN.predict(X_CNN)
-
-output_data_ANN = pd.DataFrame(predictions_ANN)  
-output_data_CNN = pd.DataFrame(predictions_CNN)
-
-output_dataset_path1 = "ANN\output_ann.csv"
-output_dataset_path2 = "CNN\output_cnn.csv"
-
-output_data_ANN.to_csv(output_dataset_path1, index=False , header = False)  
-output_data_CNN.to_csv(output_dataset_path2, index=False , header = False)
-
+model = load_model(model_path)
+data = pd.read_csv(input_dataset_path)
+X = data.values
+X = X[:, :52]
+predictions = model.predict(X)
+output_data = pd.DataFrame(predictions)
+output_data.to_csv(output_dataset_path, index=False, header=False)
